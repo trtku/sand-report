163f1f9f6c598b830dae74449b390d2eec560b17
Ko Tsuruta <trtku0809@gmail.com>
2022-07-01 12:39:33 +0900

    update import in main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from src import csv_reader as reader
 from src import csv_visualizer as vizualizer
-from visualization import stress_strain_diagram as ss
+from csv_manager import csv_to_ssd as ss
 
 if __name__=='__main__':
     smooth_factor = 10000
